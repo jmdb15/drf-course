533de3be752660ecdb5fdb26245fe28f84ec86f7
Jemuel <banagjemueld@gmail.com>
2024-10-15 17:00:31 +0800

    add something

--- library/settings.py
+++ library/settings.py
@@ -57,6 +57,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES' : [
+#         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+#     ]
+# }
+
 ROOT_URLCONF = 'library.urls'
 
 TEMPLATES = [
